cea95ce1027db146193baf25e17380d1faef3d14
lannel <jean.lannel@hotmail.com>
2018-05-23 14:00:30 +0200

    Initial commit

--- index.php
+++ index.php
@@ -1,11 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>New World OF Music</title>
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Dell
- * Date: 23/05/2018
- * Time: 09:45
- */
 
 
 
-TEST .
+
+
+
+
+
+
+
+
+
+
+
+php>
