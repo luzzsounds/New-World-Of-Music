57bf5affdadcfbbbbe82707f23decc77d0add6da
lannel <jean.lannel@hotmail.com>
2018-05-24 16:33:33 +0200

    Ajout Catégorie

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,31 +1,9 @@
 
 // Ajouter une option a un select
 
-function AjoutOptionAuSelect(this_select)
-{
-    if (this_select.value == "Autre")
-
-    {
-        var saisie;
-        var pass = false;
-        do {
-            if (pass) alert ("La valeur est incorrecte.Elle ne doit comporter que des lettres");
-
-            saisie = prompt("Entrer nouveau style de musique");
-
-            if (saisie == null) return false;
-            pass = true;
-        }
-
-        while (saisie.match(/[^a-z]/i) && saisie != "")
-
-        this_select.options[this_select.length-1] = new Option(saisie,saisie,true,true);
-
-
-
-
-    }
-
-
-
+function ajoutCategorie(){
+    var x = document.getElementById("sel");
+    var option = document.createElement("option");
+    option.text = "ValeurCategorie";
+    x.add(option);
 }
