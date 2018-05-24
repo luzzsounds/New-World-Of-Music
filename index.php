953350817e5a31cab113077af84a1ba91489a4f1
Gauthier <gauthier.cailly@laposte.net>
2018-05-24 09:54:20 +0200

    Removed the .idea folder

--- index.php
+++ index.php
@@ -4,13 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
     <title>New World OF Music</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
-
-
-
-
+<h5 id="logo">NEW WORLD OF MUSIC</h5>
 
 
 
@@ -19,24 +17,3 @@
 
 </body>
 </html>
-
-
-
-
-
-<?php
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-php>
