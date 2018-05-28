dbb75f7fb687be9a69ec00e4ab4679695921f894
Ludwig <leb.ludwig@gmail.com>
2018-05-28 14:14:07 +0200

    Création de la page des évenements et de la page detail ( et modif css )

--- evenements.php
+++ evenements.php
@@ -1,21 +1,65 @@
 <?php
-
 include('inc/pdo.php');
 include('inc/header.php'); ?>
 
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <figcaption><h4> < LES EVENEMENTS</h4></figcaption>
 
                 <div class="container-fluid bg-3 text-center">
                     <div class="row">
 
+                        <div class="ex3">
+                            <div class="row">
+                                <div class="column">
+                                    <a href="detailevents.php">
+                                        <img src="assets/img/event1.jpg" style="width:100%">
+                                    </a>
+                                    <a href="detailevents.php">
+                                        <img src="assets/img/event2.jpg" style="width:100%">
+                                    </a>
+                                    <a href="detailevents.php">
+                                        <img src="assets/img/event3.jpg" style="width:100%">
+                                    </a>
+                                    <a href="detailevents.php">
+                                        <img src="assets/img/event4.jpg" style="width:100%">
+                                    </a>
+                                    <a href="detailevents.php">
+                                        <img src="assets/img/event5.jpg" style="width:100%">
+                                    </a>
+                                    <a href="detailevents.php">
+                                        <img src="assets/img/event6.jpg" style="width:100%">
+                                    </a>
+                                </div>
+                                <div class="column">
+                                    <img src="assets/img/event4.jpg" style="width:100%">
+                                    <img src="assets/img/event5.jpg" style="width:100%">
+                                    <img src="assets/img/event6.jpg" style="width:100%">
+                                    <img src="assets/img/event1.jpg" style="width:100%">
+                                    <img src="assets/img/event2.jpg" style="width:100%">
+                                    <img src="assets/img/event3.jpg" style="width:100%">
 
-
-
-                        <div class="ex3">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</div>
+                                </div>
+                                <div class="column">
+                                    <img src="assets/img/event6.jpg" style="width:100%">
+                                    <img src="assets/img/event1.jpg" style="width:100%">
+                                    <img src="assets/img/event2.jpg" style="width:100%">
+                                    <img src="assets/img/event3.jpg" style="width:100%">
+                                    <img src="assets/img/event4.jpg" style="width:100%">
+                                    <img src="assets/img/event5.jpg" style="width:100%">
+                                </div>
+                                <div class="column">
+                                    <img src="assets/img/event1.jpg" style="width:100%">
+                                    <img src="assets/img/event2.jpg" style="width:100%">
+                                    <img src="assets/img/event3.jpg" style="width:100%">
+                                    <img src="assets/img/event4.jpg" style="width:100%">
+                                    <img src="assets/img/event5.jpg" style="width:100%">
+                                    <img src="assets/img/event6.jpg" style="width:100%">
+                                </div>
+                            </div>
+                        </div>
 
 
                     </div>
@@ -25,24 +69,6 @@ include('inc/header.php'); ?>
 
 
 
-            <div class="col-lg-6">
-
-                <figcaption><h4> CHERCHER UN EVENEMENT></h4></figcaption>
-                <div class="grid">
-
-                    <form action="" method="get" class="search">
-
-                        <div class="form__field">
-                            <input type="search" name="search" placeholder="Concert,Festival... Code postal?" class="form__input">
-                            <input type="submit" value="Recherche" class="button">
-                        </div>
-
-                    </form>
-
-
-
-                </div>
-            </div>
         </div>
     </div>
 
