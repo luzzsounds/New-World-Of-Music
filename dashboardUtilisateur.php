8a3a1a44f771757a0722df6755446d5cecdc9d62
Ludwig <leb.ludwig@gmail.com>
2018-05-30 09:14:16 +0200

    Création du dashboard utilisateur ( manque le calendrier )

---
+++ dashboardUtilisateur.php
@@ -0,0 +1,50 @@
+<?php
+include('inc/header.php'); ?>
+
+<div class="container">
+    <div class="row profile">
+        <div class="col-md-3">
+            <div class="profile-sidebar">
+
+                <div class="profile-usertitle">
+                    <div class="profile-usertitle-name">
+                        Ludwig lebrun
+                    </div>
+                    <div class="profile-usertitle-job">
+                        blablabla
+                    </div>
+                </div>
+
+                <div class="profile-userbuttons">
+                    <button type="button" class="btn btn-success btn-sm">Mes Évenements</button>
+                    <button type="button" class="btn btn-danger btn-sm">Modifier mon profil</button>
+                </div>
+                <!-- END SIDEBAR BUTTONS -->
+                <!-- SIDEBAR MENU -->
+                <div class="profile-usermenu">
+                    <ul class="nav">
+                        <li class="active">
+                            <a href="#">
+                                <i class="glyphicon glyphicon-home"></i>
+                                Contact support </a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="profile-content">
+
+               PLACE du
+                <a href="https://teamup.com/ksvxyo68wfaazepgt9">calendrier</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include('inc/footer.php');?>
+
+
+
